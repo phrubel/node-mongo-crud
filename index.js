ad66c4b6ffe9f451dd4bc1f872d1f2b2792dff13
phrubel <phrubel42@gmail.com>
2021-03-25 16:40:01 +0600

    delete function issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-const bodyParser=require('body-parser')
+const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 
@@ -16,53 +16,53 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+'/index.html');
+  res.sendFile(__dirname + '/index.html');
 })
 
 client.connect(err => {
-    const ProductCollection = client.db("organicdb").collection("products");
+  const ProductCollection = client.db("organicdb").collection("products");
 
-    app.get('/products',(req,res) =>{
-      ProductCollection.find({})
-      .toArray((err,documents) =>{
+  app.get('/products', (req, res) => {
+    ProductCollection.find({})
+      .toArray((err, documents) => {
         res.send(documents)
-      })  
-    })
+      })
+  })
 
-    app.get('/product/:id',(req,res)=>{
-      ProductCollection.find({_id: ObjectId(req.params.id)})
-      .toArray((err,documents) =>{
+  app.get('/product/:id', (req, res) => {
+    ProductCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
         res.send(documents[0])
       })
-    })
+  })
 
-    app.patch('/update/:id',(req,res) =>{
-      ProductCollection.updateOne({_id: ObjectId(req.params.id)},
-     
+  app.patch('/update/:id', (req, res) => {
+    ProductCollection.updateOne({ _id: ObjectId(req.params.id) },
+
       {
-        $set:{price: req.body.price, quantity: req.body.quantity}
+        $set: { price: req.body.price, quantity: req.body.quantity }
       })
-      .then(result =>{
-        res.send(result.modifiedCount >0)
+      .then(result => {
+        res.send(result.modifiedCount > 0)
       })
-      
-    })
-   
-    app.post('/addProduct', (req, res) => {
-       const product =req.body;
-       ProductCollection.insertOne(product)
-       .then(result =>{
-           console.log('data aded success');
-           res.redirect('/')
-       })
-    })
 
-    app.delete('/delete/:id', (req,res) =>{
-      ProductCollection.deleteOne({_id: ObjectId(req.params.id)})
-      .then(result=>{
-       res.send(result.deletedCount > 0);
+  })
+
+  app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    ProductCollection.insertOne(product)
+      .then(result => {
+        console.log('data aded success');
+        res.redirect('/')
       })
-    })
+  })
+
+  app.delete('/delete/:id', (req, res) => {
+    ProductCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        res.send(result.deletedCount > 0);
+      })
+  })
 
 });
 
